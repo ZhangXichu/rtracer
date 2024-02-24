81ec72f738c571df5927efdee8926abeb9ce9406
ZhangXichu <xichu.zhang@yahoo.com>
2024-02-24 22:35:49 +0100

    use ray to make graduate blue color

--- src/main.cpp
+++ src/main.cpp
@@ -3,32 +3,43 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/matx.hpp>
 
+#include "ray.hpp"
+#include "utils.hpp"
+
 int main()
 {
     int num_cols = 400;
     int num_rows = 300;
 
-    std::cout << "P3\n" << num_cols << " " << num_rows << "\n255\n";
+    // std::cout << "P3\n" << num_cols << " " << num_rows << "\n255\n";
 
     cv::Mat img(num_rows, num_cols, CV_8UC3);
+
+    cv::Vec3d lower_left(3, 0, 0);
+    cv::Vec3d horizontal(1.5, 6, 0);
+    cv::Vec3d vertical(-1.5, 2, 0);
+    cv::Vec3d center(1.5, 2, 0);
 
     for (int i = 0; i < num_rows; i++ ) {
         for (int j = 0; j < num_cols; j++) {
             // cv::Vec3d v(i/num_rows, j/num_rows, 0.2);
-            double r = double(i) / num_rows;
-            double g = double(j) / num_cols;
-            double b = 0.2;
+            double u = double(i) / num_rows;
+            double v = double(j) / num_cols;
+
+            Ray ray(center, lower_left + u*horizontal + v*vertical);
+
+            cv::Vec3d col = color(ray);
 
             // convert color values from double (0.0 to 1.0) to int (0 to 255)
-            int ir = static_cast<int>(255.99 * r);
-            int ig = static_cast<int>(255.99 * g);
-            int ib = static_cast<int>(255.99 * b);
+            int ir = static_cast<int>(255.99 * col[0]);
+            int ig = static_cast<int>(255.99 * col[1]);
+            int ib = static_cast<int>(255.99 * col[2]);
 
             img.at<cv::Vec3b>(i, j) = cv::Vec3b(ib, ig, ir);
         }
     }
 
-    cv::imwrite("/workspace/output/output_image.png", img);
+    cv::imwrite("/workspace/output/blue_grad.png", img);
 
     return 0;
 }
--- src/utils.cpp
+++ src/utils.cpp
@@ -6,8 +6,6 @@ cv::Vec3d unit_vector(const cv::Vec3d v)
 {
     double length = cv::norm(v);
 
-    std::cout << "The length of v is: " << length << std::endl;
-
     return v / length;
 }
 
@@ -17,7 +15,7 @@ cv::Vec3d color(const Ray& r)
     double t = 0.5 * (unit_direction[1] + 1.0); // normalize y-axis range to [0, 1]
 
     cv::Vec3d v_e(1.0, 1.0, 1.0); // white
-    cv::Vec3d v(0.5, 0.7, 1.0); // light blue
+    cv::Vec3d v(0.6, 0.7, 1.0); // light blue
 
     return (1.0-t)*v_e + t*v; // linear interpolation
 }
